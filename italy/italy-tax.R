23a758559fe9b54f5bde6be76a1c41c76d3298fc
rapsoj <jessicakristenr@gmail.com>
2024-04-24 13:50:13 +0100

    Fixed error with calculating marginal taxes

--- italy/italy-tax.R
+++ italy/italy-tax.R
@@ -12,10 +12,10 @@ df <- data.frame(gross_income = seq(1000, 400000, by = 1000)) %>%
   mutate(
     # Simulate personal income tax
     irpef = case_when(
-      gross_income > 0 ~ gross_income * 0.23,
-      gross_income > 15000 ~ gross_income * 0.27,
-      gross_income > 28000 ~ gross_income * 0.38,
-      gross_income > 55000 ~ gross_income * 0.41,
-      gross_income > 75000 ~ gross_income * 0.42
+      gross_income <= 15000 ~ gross_income * 0.23,
+      gross_income <= 28000 ~ (15000 * 0.23) + ((gross_income - 15000) * 0.27),
+      gross_income <= 55000 ~ (15000 * 0.23) + (13000 * 0.27) + ((gross_income - 28000) * 0.38),
+      gross_income <= 75000 ~ (15000 * 0.23) + (13000 * 0.27) + (27000 * 0.38) + ((gross_income - 55000) * 0.41),
+      TRUE ~ (15000 * 0.23) + (13000 * 0.27) + (27000 * 0.38) + (20000 * 0.41) + ((gross_income - 75000) * 0.42)
     )
   )
